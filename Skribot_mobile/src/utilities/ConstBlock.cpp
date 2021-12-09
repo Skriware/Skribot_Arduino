4c62410d3e2cb853beda06dd337d8e5877032ecf
Lukkoz <ukikoza@gmail.com>
2021-12-09 15:23:41 +0100

    Update ConstBlock.cpp

--- Skribot_mobile/src/utilities/ConstBlock.cpp
+++ Skribot_mobile/src/utilities/ConstBlock.cpp
@@ -1,20 +1,17 @@
 #include "ConstBlock.h"
 
 ConstBlock::ConstBlock(int id, int32_t value){
-	blockID = id;
-	output = value;
-	  input_block 	= NULL;
-  	next 			= NULL;
-  	output_block 	= NULL;
-  	inputblockID 	= 0;
-  	outputblockID 	= 0;
-  	actionID 		= 0;
-  	nextblockID 	= 0;
+  blockID = id;
+  output = value;
+    input_block   = NULL;
+    next      = NULL;
+    output_block  = NULL;
+    inputblockID  = 0;
+    outputblockID   = 0;
+    actionID    = 0;
+    nextblockID   = 0;
     t_output = NULL;
     N_output = 1;
-    tmp_8 = NULL;
-    tmp_16 = NULL;
-
 }
 
 ConstBlock::ConstBlock(int id, int32_t *value,byte N){
@@ -29,16 +26,9 @@ ConstBlock::ConstBlock(int id, int32_t *value,byte N){
     nextblockID   = 0;
     t_output = value;
     N_output = N;
-    tmp_8 = NULL;
-    tmp_16 = NULL;
-
 }
 
-ConstBlock::~ConstBlock(){
-  if(tmp_8    != NULL)delete tmp_8;
-  if(tmp_16   != NULL)delete tmp_16;
-  //if(t_output != NULL)delete t_output;
-}
+ConstBlock::~ConstBlock(){}
 
 void ConstBlock::setTableOutput(int32_t *tmp, byte N){
   t_output = tmp;
@@ -51,12 +41,12 @@ byte ConstBlock::get_output_N(){return(N_output);}
 
 byte* ConstBlock::get_table_output_8(){
   if(N_output > 0){
-  tmp_8 = new uint8_t[N_output]; 
+  uint8_t *tmp = new uint8_t[N_output]; 
   for(byte rr = 0; rr<N_output;rr++){
-    tmp_8[rr] = (uint8_t)t_output[rr];
+    tmp[rr] = (uint8_t)t_output[rr];
 
   }
-  return(tmp_8);
+  return(tmp);
 }else{
   return(NULL);
 }
@@ -65,11 +55,11 @@ byte* ConstBlock::get_table_output_8(){
 
 int16_t* ConstBlock::get_table_output_16() {
   if(N_output > 0) {
-    tmp_16 = new int16_t[N_output]; 
+    int16_t *tmp = new int16_t[N_output]; 
     for(byte rr = 0; rr<N_output;rr++) {
-      tmp_16[rr] = (int16_t) t_output[rr];
+      tmp[rr] = (int16_t) t_output[rr];
   }
-  return(tmp_16);
+  return(tmp);
 }else{
   return(NULL);
 }
@@ -77,9 +67,9 @@ int16_t* ConstBlock::get_table_output_16() {
 }
 
 /*ConstBlock::ConstBlock(int id , String value){
-	blockID = id;
-	string_output = value;
-	input_block = NULL;
+  blockID = id;
+  string_output = value;
+  input_block = NULL;
   next = NULL;
   output_block = NULL;
 }
